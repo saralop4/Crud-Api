c25b6354f523604d44e9f159a4df714c2da0ec9b
saralop4 <saralopezjb16@gmail.com>
2024-08-23 10:43:35 -0500

    endpoint de guardado funcionando

--- Crud.Api/database/migrations/2024_08_22_205246_create_estudiante_table.php
+++ Crud.Api/database/migrations/2024_08_22_205246_create_estudiante_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('estudiante', function (Blueprint $table) {
+        Schema::create('estudiantes', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
             $table->string('correo');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('estudiante');
+        Schema::dropIfExists('estudiantes');
     }
 };
--- Crud.Api/routes/api.php
+++ Crud.Api/routes/api.php
@@ -4,4 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller\EstudianteController;
 
-Route::get('/GetEstudiante', [EstudianteController::class, 'index']);
+Route::get('/GetEstudiante', [EstudianteController::class, 'Index']);
+Route::post('/CreateEstudiante', [EstudianteController::class, 'Create']);
+
